300fa000c980ed723803196bffa105523b383c79
Johnny Debebe <28043284+bjohnnyd@users.noreply.github.com>
2019-12-28 13:23:40 +0000

    developing parser

--- src/netmhcpan/parser.rs
+++ src/netmhcpan/parser.rs
@@ -13,21 +13,6 @@ fn hla_start(input: &str) -> IResult<&str, &str> {
     tag("HLA-")(input)
 }
 
-fn optional_asterisk(input: &str) -> IResult<&str, Option<char>> {
-    opt(char('*'))(input)
-}
-
-fn colon(input: &str) -> IResult<&str, char> {
-    char(':')(input)
-}
-fn is_gene_locus(input: &str) -> IResult<&str, char> {
-    alt((char('A'), char('B'), char('C')))(input)
-}
-
-fn take_digits(input: &str) -> IResult<&str, &str> {
-    digit1(input)
-}
-
 fn take_until_space(input: &str) -> IResult<&str, &str> {
     is_not(" ")(input)
 }
@@ -36,12 +21,16 @@ fn take_until_digit(input: &str) -> IResult<&str, &str> {
     take_while(|c| !char::is_digit(c, 10))(input)
 }
 
-fn take_nn(input: &str) -> IResult<&str, f32> {
+fn take_float(input: &str) -> IResult<&str, f32> {
     map_res(take_while(|c| char::is_digit(c, 10) || c == '.'), as_float)(input)
 }
 
 fn as_float(input: &str) -> Result<f32, std::num::ParseFloatError> {
     input.parse::<f32>()
+}
+
+fn is_commented(input: &str) -> IResult<&str, char> {
+    char('#')(input)
 }
 
 //fn (input: &str) -> IResult<&str, char> {
@@ -57,7 +46,7 @@ mod tests {
         let (i, _) = hla_start(hla).unwrap();
         let (i, hla) = take_until_space(i).unwrap();
         let (i, pre_digit) = take_until_digit(i).unwrap();
-        let (i, nn) = take_nn(i).unwrap();
+        let (i, nn) = take_float(i).unwrap();
         dbg!(i);
         dbg!(nn);
     }

00982c955c76fff923536c12a1ce0499a282f884
Johnny Debebe <28043284+bjohnnyd@users.noreply.github.com>
2020-04-17 16:37:55 +0100

    thinking of alternative ligand information storage and obtaining

---
+++ immunoprot/build.rs
@@ -0,0 +1,8 @@
+
+
+fn main() {
+
+    let lg_info = include_str!("src/resources/2019-12-29_lg.tsv");
+}
+
+
--- immunoprot/src/lib.rs
+++ immunoprot/src/lib.rs
@@ -9,3 +9,17 @@ pub mod ig_like {
     pub mod kir;
     pub mod lilrb;
 }
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_ligand_info() {
+        let lg_info = include_str!("resources/2019-12-29_lg.tsv");
+
+        lg_info
+            .lines()
+            .for_each(|l|{
+                dbg!(l);
+            });
+    }
+}

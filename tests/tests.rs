8773b60638ab9852f8b3b79b4909069addfb0854
Johnny Debebe <28043284+bjohnnyd@users.noreply.github.com>
2020-05-30 19:37:30 +0100

    started working on tests

---
+++ tests/tests.rs
@@ -0,0 +1,29 @@
+use assert_cmd::prelude::*;
+use predicates::str::{contains, is_match};
+use std::process::Command;
+
+#[test]
+// No arguments should mention help and required field
+fn cli_no_args_help() {
+    Command::cargo_bin("fs-tool")
+        .unwrap()
+        .assert()
+        .stderr(contains("help"))
+        .stderr(contains("--binding_predictions"))
+        .stderr(contains("--output"));
+}
+
+#[test]
+// Should throw an error for incorrect binding prediction file
+fn cli_wrong_binding_predictions() {
+    Command::cargo_bin("fs-tool")
+        .unwrap()
+        .args(&[
+            "-b",
+            "tests/input/binding_predictions/large_cohort_batch1.txt",
+            "-o",
+            "tests/output/alleles",
+        ])
+        .assert()
+        .stderr(contains("No such file"));
+}

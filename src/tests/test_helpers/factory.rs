30d192022eeaa971bf1d75dc3ff99d6841718441
Johnny Debebe <28043284+bjohnnyd@users.noreply.github.com>
2021-06-16 18:54:27 +0100

    factory for peptides and binding info created

--- src/tests/test_helpers/factory.rs
+++ src/tests/test_helpers/factory.rs
@@ -1,12 +1,20 @@
 use immunoprot::ig_like::kir_ligand::KirLigandMap;
 use netmhcpan::result::{BindingInfo, Peptide};
-use rand::{prelude::SliceRandom, Rng};
+use rand::{distributions::Alphanumeric, prelude::SliceRandom, Rng};
+
+const AA: [&str; 20] = [
+    "A", "R", "N", "D", "C", "Q", "E", "G", "H", "I", "L", "K", "M", "F", "P", "S", "T", "W", "Y",
+    "V",
+];
+
+const IDENTITY: &str = "Protein";
+const ALIGNMENT_MODS: [usize; 12] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
 
 pub struct AlleleFactory {
     kir_ligand_map: KirLigandMap,
 }
 
-enum RankThreshold {
+pub enum RankThreshold {
     Strong,
     Weak,
 }
@@ -16,7 +24,10 @@ pub struct BindingFactory {
 }
 
 impl BindingFactory {
-    fn generate_binding_info(
+    pub fn new(peptides: Vec<Peptide>) -> Self {
+        Self { peptides }
+    }
+    pub fn generate_binding_info(
         &self,
         n: usize,
         rank_class: Option<RankThreshold>,
@@ -49,14 +60,48 @@ impl BindingFactory {
     }
 }
 
-pub fn generate_peptides(n: usize) -> Vec<Peptide> {
-    todo!()
+pub fn generate_peptides(n: usize, length: usize) -> Vec<Peptide> {
+    let mut aa = AA.clone();
+    let mut rng = rand::thread_rng();
+
+    (0..n)
+        .map(|_| {
+            aa.shuffle(&mut rng);
+            let seq = aa.iter().cloned().take(length).collect::<String>();
+            Peptide::new(
+                0,
+                seq.clone(),
+                IDENTITY.to_string(),
+                seq.to_string(),
+                &ALIGNMENT_MODS,
+            )
+        })
+        .collect()
 }
 
 #[cfg(test)]
 mod tests {
 
     use super::*;
+
+    #[test]
+    fn test_peptide_generation() {
+        let peptides = generate_peptides(3, 9);
+        assert_eq!(peptides.len(), 3);
+        assert!(peptides.iter().all(|peptide| peptide.len() == 9));
+    }
+
+    #[test]
+    fn test_binding_info_generation() {
+        let peptides = generate_peptides(5, 9);
+        let binding_factory = BindingFactory::new(peptides);
+
+        let binding_infos = binding_factory.generate_binding_info(10, Some(RankThreshold::Strong));
+
+        assert!(binding_infos
+            .iter()
+            .all(|binding_info| binding_info.rank < 0.5));
+    }
 
     #[test]
     fn test_allele_factory_generation() {

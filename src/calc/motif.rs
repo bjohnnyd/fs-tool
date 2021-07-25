0efafe130b692758a4f9871e03c15808225b0422
Johnny Debebe <28043284+bjohnnyd@users.noreply.github.com>
2021-07-25 10:12:04 +0100

    implemented basic function to extract motif

--- src/calc/motif.rs
+++ src/calc/motif.rs
@@ -15,6 +15,20 @@ impl Measure {
             .map(|digit| digit.parse::<usize>())
             .collect::<Result<Vec<_>, _>>()?)
     }
+
+    fn get_motif(&self, peptide: &str) -> Vec<u8> {
+        let pep_len = peptide.len();
+        self.motif_pos
+            .iter()
+            .flat_map(|idx| {
+                if *idx < pep_len {
+                    Some(peptide.as_bytes()[*idx])
+                } else {
+                    None
+                }
+            })
+            .collect()
+    }
 }
 
 impl std::str::FromStr for Measure {
@@ -43,5 +57,46 @@ impl std::str::FromStr for Measure {
             }
         }
         Ok(Self { name, motif_pos })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_get_motif() {
+        let input_measure = "CD8:2,3,4,5,6,9";
+        let measure = input_measure.parse::<Measure>().unwrap();
+
+        let input_peptide = "ABCDEFGHIJ";
+        let expected = input_peptide
+            .bytes()
+            .enumerate()
+            .filter(|(i, _)| measure.motif_pos.contains(i))
+            .map(|(_, aa)| aa)
+            .collect::<Vec<u8>>();
+
+        let motif = measure.get_motif(&input_peptide);
+
+        assert_eq!(motif, expected);
+    }
+
+    #[test]
+    fn test_get_motif_peptide_too_short() {
+        let input_measure = "CD8:2,3,4,5,6,9";
+        let measure = input_measure.parse::<Measure>().unwrap();
+
+        let input_peptide = "ABCDEFGH";
+        let expected = input_peptide
+            .bytes()
+            .enumerate()
+            .filter(|(i, _)| measure.motif_pos.contains(i))
+            .map(|(_, aa)| aa)
+            .collect::<Vec<u8>>();
+
+        let motif = measure.get_motif(&input_peptide);
+
+        assert_eq!(motif, expected);
     }
 }
